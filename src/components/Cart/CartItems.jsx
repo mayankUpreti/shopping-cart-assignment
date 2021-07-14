cd92f5dc2edfbd28cfa73f0e6a1f189acaf397d5
mayank <mayank.u@quillhash.com>
2021-07-14 17:23:39 +0530

    added button in cart

--- src/components/Cart/CartItems.jsx
+++ src/components/Cart/CartItems.jsx
@@ -10,7 +10,7 @@ const CartItem=({data,cartitem,setCartItem})=>{
             <div className='col-7 d-flex flex-column justify-content-center'>
                 <div className='font-weight-bold'>{data.name}</div>
                 <div className='d-flex justify-content-around align-items-center'>
-                   <span data-testid='cart-decrease' tabIndex={0} role='button' onClick={()=>data.quantity>1?addItemToCard(data,'remove',cartitem,setCartItem):removeItemfromUI(data,cartitem,setCartItem)}>-</span><span data-testid="cart-quantity" className='font-weight-bold'>{data.quantity}</span> <span data-testid='cart-increase'  tabIndex={0} role='button' onClick={()=>addItemToCard(data,'add',cartitem,setCartItem)}>+ </span> x <span> Rs.{data.price}</span>
+                   <span className='cart-btn' data-testid='cart-decrease' tabIndex={0} role='button' onClick={()=>data.quantity>1?addItemToCard(data,'remove',cartitem,setCartItem):removeItemfromUI(data,cartitem,setCartItem)}>-</span><span data-testid="cart-quantity" className='font-weight-bold'>{data.quantity}</span> <span data-testid='cart-increase' className='cart-btn' tabIndex={0} role='button' onClick={()=>addItemToCard(data,'add',cartitem,setCartItem)}>+ </span> x <span> Rs.{data.price}</span>
                 </div>
             </div>
             <div className='col-2 d-flex justify-content-center align-items-end p-3'>
